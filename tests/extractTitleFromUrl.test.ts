ef70bbc5942aac4e0cf1333c2d95a5869c82a34b
Sam <60025491+SamSumiya@users.noreply.github.com>
2025-04-20 22:53:20 -0700

    Add unit tests for extractTitleFromUrl utility

---
+++ tests/extractTitleFromUrl.test.ts
@@ -0,0 +1,13 @@
+import { extractTitleFromUrl } from '../src/utils/extractTitleFromUrl';
+
+describe('extractTitleFromUrl', () => {
+  it('should convert valid LeetCode URLs to formatted titles', () => {
+    expect(extractTitleFromUrl('https://leetcode.com/problems/two-sum/')).toBe('Two Sum');
+  });
+
+  it('should return "Unknown Title" for malformed or invalid URLs', () => {
+    expect(extractTitleFromUrl('')).toBe('Unknown Title');
+    expect(extractTitleFromUrl('not-a-leetcode-url')).toBe('Unknown Title');
+    expect(extractTitleFromUrl('leetcode.com/problems/')).toBe('Unknown Title');
+  });
+});
