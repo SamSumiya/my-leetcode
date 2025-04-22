a5a3937cece8bd2989a0982e8bf082d2bc3374aa
Sam <60025491+SamSumiya@users.noreply.github.com>
2025-04-22 11:12:19 -0700

    Seperated buildLogEntry out of leetcode-log to its own file under utiles

---
+++ src/utils/buildLogEntry.ts
@@ -0,0 +1,10 @@
+import { extractTitleFromUrl } from './extractTitleFromUrl';
+import type { LogEntry, LogEntryInput } from '../types';
+
+export function buildLogEntry(response: LogEntryInput): LogEntry {
+  return {
+    ...response,
+    title: extractTitleFromUrl(response.url),
+    dateOption: response.dateOption,
+  };
+}
