249227f88cb7acbab11969798cc4d68e4244fe12
Sam <60025491+SamSumiya@users.noreply.github.com>
2025-04-22 11:49:00 -0700

    added a case that url is empty and updated extractTitleFromUUrl by removing try catch block as it is not neccesary because match does not throw

--- tests/buildLogEntry.test.ts
+++ tests/buildLogEntry.test.ts
@@ -12,10 +12,33 @@ describe('buildLogEntry', () => {
     dateOption: 'today',
   };
 
+  const inputWithoutURL: LogEntryInput = {
+    url: '',
+    difficulty: 'Easy',
+    status: '✅ Pass',
+    approach: 'Hash map',
+    tags: ['array', 'hashmap'],
+    starred: false,
+    dateOption: 'today',
+  };
+
   it('Should return correct response object from input data', () => {
     expect(buildLogEntry(input)).toEqual({
       title: 'Two Sum',
       url: 'https://leetcode.com/problems/two-sum',
+      difficulty: 'Easy',
+      status: '✅ Pass',
+      approach: 'Hash map',
+      tags: ['array', 'hashmap'],
+      starred: false,
+      dateOption: 'today',
+    });
+  });
+
+  it('Should be Unknown Title if user did not provide an url', () => {
+    expect(buildLogEntry(inputWithoutURL)).toEqual({
+      title: 'Unknown Title',
+      url: '',
       difficulty: 'Easy',
       status: '✅ Pass',
       approach: 'Hash map',
