36c9d5b8e79cb76d9b44b8be422a39335e846640
Sam <60025491+SamSumiya@users.noreply.github.com>
2025-04-24 09:17:21 -0700

    changed function name to writeLogToJsonlFile

--- src/utils/writeLogToFile.ts
+++ src/utils/writeLogToFile.ts
@@ -2,7 +2,7 @@ import { writeFile } from 'fs/promises';
 
 import type { LogEntry } from '../types';
 
-export async function writeLogToFile(filePath: string, log: LogEntry[]): Promise<void> {
+export async function writeLogToJsonlFile(filePath: string, log: LogEntry[]): Promise<void> {
   try {
     await writeFile(filePath, JSON.stringify(log, null, 2), 'utf-8');
   } catch (err) {
--- tests/writeLogToFile.test.ts
+++ tests/writeLogToFile.test.ts
@@ -1,4 +1,4 @@
-import { writeLogToFile } from '../src/utils/writeLogToFile';
+import { writeLogToJsonlFile } from '../src/utils/writeLogToFile';
 import { writeFile } from 'fs/promises';
 
 import type { LogEntry } from '../src/types';
@@ -11,7 +11,7 @@ describe('writeLogToFile', () => {
   it('Should call writeFile with inputs path and log object', async () => {
     const fakePath = '';
     const fakeData: LogEntry[] = [];
-    await writeLogToFile(fakePath, fakeData);
+    await writeLogToJsonlFile(fakePath, fakeData);
 
     expect(writeFile).toHaveBeenCalledWith(fakePath, JSON.stringify(fakeData, null, 2), 'utf-8');
   });
