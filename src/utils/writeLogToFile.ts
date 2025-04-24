909564a7031e9ea60a85cb5c4e970d51e74de01b
Sam <60025491+SamSumiya@users.noreply.github.com>
2025-04-24 10:01:06 -0700

    changed writelogs to append and json to jsonl file

--- src/utils/writeLogToFile.ts
+++ src/utils/writeLogToFile.ts
@@ -1,10 +1,10 @@
 import { writeFile } from 'fs/promises';
-
+import { appendFile } from 'fs/promises';
 import type { LogEntry } from '../types';
 
-export async function writeLogToJsonlFile(filePath: string, log: LogEntry[]): Promise<void> {
+export async function writeLogToJsonlFile(filePath: string, log: LogEntry): Promise<void> {
   try {
-    await writeFile(filePath, JSON.stringify(log, null, 2), 'utf-8');
+    await appendFile(filePath, JSON.stringify(log) + '\n', 'utf-8');
   } catch (err) {
     console.error('Error writing log to file:', err);
   }
--- tests/writeLogToFile.test.ts
+++ tests/writeLogToFile.test.ts
@@ -10,7 +10,16 @@ jest.mock('fs/promises', () => ({
 describe('writeLogToFile', () => {
   it('Should call writeFile with inputs path and log object', async () => {
     const fakePath = '';
-    const fakeData: LogEntry[] = [];
+    const fakeData: LogEntry = {
+      title: 'Fake Title',
+      url: 'https://leetcode.com/problems/fake',
+      difficulty: 'Easy',
+      status: '✅ Pass',
+      approach: 'Brute force',
+      tags: ['array'],
+      starred: false,
+      dateOption: 'today',
+    };
     await writeLogToJsonlFile(fakePath, fakeData);
 
     expect(writeFile).toHaveBeenCalledWith(fakePath, JSON.stringify(fakeData, null, 2), 'utf-8');
