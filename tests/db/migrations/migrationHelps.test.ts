337193bca10e4c22da677d2e05015d5142609afd
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-04-28 13:48:46 -0700

    added return content from files test

--- tests/db/migrations/migrationHelps.test.ts
+++ tests/db/migrations/migrationHelps.test.ts
@@ -10,7 +10,7 @@ import { readMigrationFile } from '../../../src/db/migrationHelps';
 
 jest.mock('fs', () => ({
   readdirSync: jest.fn().mockReturnValue(['010file', '001file', '002file']),
-  readFileSync: jest.fn(),
+  readFileSync: jest.fn().mockReturnValue('fake contents here!'),
 }));
 
 describe('Testing migration helper functions', () => {
@@ -26,11 +26,18 @@ describe('Testing migration helper functions', () => {
     expect(fs.readdirSync).toHaveBeenCalledWith('fake/path');
   });
 
-  it('Should return absolute path for each migration file', () => {
+  it('should return absolute path for each migration file', () => {
     const absPath = path.join(__dirname, '../../../src/migrations');
     const fileName = '/fakePath';
     const pathName = getMigrationFileAbsPath(absPath, fileName);
     const expectedPath = path.resolve(absPath, fileName);
     expect(pathName).toBe(expectedPath);
+  });
+
+  it('should return file and return the content', () => {
+    const fakePath = 'fake/path';
+    const content = readMigrationFile(fakePath);
+    expect(content).toBe('fake contents here!');
+    expect(fs.readFileSync).toHaveBeenCalledWith(fakePath, 'utf-8');
   });
 });
