ad1b3106c08d008814c77c9c5f0c6829424c3bca
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2025-05-09 15:20:16 -0700

    added code to sanitize logs and seed logs table from jsonl file

--- src/cli/dev/seedLogsTableFromJsonl.dev.ts
+++ src/cli/dev/seedLogsTableFromJsonl.dev.ts
@@ -1,12 +1,26 @@
 import readline from 'readline';
 import fs from 'fs';
 
-// import { LogEntryMeta } from '../../types';
+import pool from '../../db';
+
+import { LogEntryMeta } from '../../types';
 
 import { resolveFilePath } from '../../utils/resolveFilePath';
 import { parseFlags } from '../../utils/parseFlags';
+import { sanitizeLogs } from '../../utils/sanitize/sanitizeLog';
+
+async function insertIntoDB(logData: LogEntryMeta): Promise<void> {
+  await pool.query(
+    `
+        INSERT INTO logs ( slug, status, approach, starred )
+        VALUES ( $1, $2, $3, $4) 
+    `,
+    [logData.slug, logData.status, logData.approach, logData.starred]
+  );
+}
 
 async function main() {
+  let invalidLogCount = 0;
   const args = process.argv.slice(0);
   const filePath = parseFlags(args);
   const path = resolveFilePath(filePath.file);
@@ -19,13 +33,21 @@ async function main() {
   for await (const line of rl) {
     try {
       const parsedLine = JSON.parse(line);
-      const logData = {
-        ...parsedLine,
-      };
+      const sanitizedData = sanitizeLogs(parsedLine);
+      if (!sanitizedData) {
+        invalidLogCount++;
+        continue;
+      }
+      await insertIntoDB(sanitizedData);
     } catch (err) {
       console.error(`❌ Faild to read line - ${err}`);
     }
   }
+  rl.close();
+  console.log(`
+    ⚠️ Skipped ${invalidLogCount} invalid ${invalidLogCount > 1 ? 'entries' : 'entry'}
+    `);
+  await pool.end();
 }
 
 main()
